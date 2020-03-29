84d2b445c1e2d6c78ce0dc6e05280463df4207d6
Stefan Eicher <stefan.eicher@gmail.com>
2020-03-29 21:54:44 +0200

    update build scripts
    (cherry picked from commit f75cf3c8f5c24112178f9ab57759a5e5b99b5f4c)

--- build.sh
+++ build.sh
@@ -1,24 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 docker build . -t stm32-cmake-stefaneicher_build
 
-#cd "$(dirname "$0")"
-cd stm32-blinky
-
-rm -rf cmake-build-debug
-mkdir cmake-build-debug
-
-cd cmake-build-debug || exit
-
-cmake -DCMAKE_BUILD_TYPE=Debug  \
--DTOOLCHAIN_PREFIX=/opt/toolchain-arm/gcc-arm-none-eabi-9-2019-q4-major \
--DSTM32Cube_DIR=/opt/stm32cubeF4 \
--DSTM32_CHIP=STM32F411RE \
--DSTM32_FAMILY=F4 \
--DCMAKE_TOOLCHAIN_FILE=../cmake/gcc_stm32.cmake \
--G "CodeBlocks - Unix Makefiles" ..
-
-#cmake --build . --target all -- -j 4
-cmake --build . --target stm32-blinky -- -j 4
-cmake --build . --target stm32-blinky.bin -- -j 4
-cmake --build . --target stm32-blinky.hex -- -j 4
+docker run -i --rm --net=host  \
+--volume $PWD:/$PWD \
+stm32-cmake-stefaneicher_build \
+bash \
+-c "cd /$PWD && ls -l && ./buildStm32F411RE.sh" \
