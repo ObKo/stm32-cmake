46d652a5b912ccdff094b233ff58dd6babcedf34
sergeyblagodarnyy <sergey.blagodarnyy@faramax.com>
2021-07-06 17:25:46 +0300

    Alignment in the utilities file

--- cmake/stm32/utilities.cmake
+++ cmake/stm32/utilities.cmake
@@ -57,77 +57,77 @@ FetchContent_Declare(
 set(IDX 0)
 foreach(FAMILY ${STM32_FETCH_FAMILIES})
     string(TOLOWER ${FAMILY} FAMILY_L)
-	list(GET STM32_FETCH_CUBE_VERSIONS ${IDX} CUBE_VERSION)
-	list(GET STM32_FETCH_CMSIS_VERSIONS ${IDX} CMSIS_VERSION)
-	list(GET STM32_FETCH_HAL_VERSIONS ${IDX} HAL_VERSION)
-	
-	FetchContent_Declare(
-		STM32Cube${FAMILY}
-		GIT_REPOSITORY https://github.com/STMicroelectronics/STM32Cube${FAMILY}/
-		GIT_TAG        ${CUBE_VERSION}
-		GIT_PROGRESS   TRUE
-	)
-	FetchContent_Declare(
-		STM32-CMSIS-${FAMILY}
-		GIT_REPOSITORY https://github.com/STMicroelectronics/cmsis_device_${FAMILY_L}/
-		GIT_TAG        ${CMSIS_VERSION}
-		GIT_PROGRESS   TRUE
-	)
-	FetchContent_Declare(
-		STM32-HAL-${FAMILY}
-		GIT_REPOSITORY https://github.com/STMicroelectronics/stm32${FAMILY_L}xx_hal_driver/
-		GIT_TAG        ${HAL_VERSION}
-		GIT_PROGRESS   TRUE
-	)
-	math(EXPR IDX "${IDX} + 1")
+   list(GET STM32_FETCH_CUBE_VERSIONS ${IDX} CUBE_VERSION)
+   list(GET STM32_FETCH_CMSIS_VERSIONS ${IDX} CMSIS_VERSION)
+   list(GET STM32_FETCH_HAL_VERSIONS ${IDX} HAL_VERSION)
+
+   FetchContent_Declare(
+      STM32Cube${FAMILY}
+      GIT_REPOSITORY https://github.com/STMicroelectronics/STM32Cube${FAMILY}/
+      GIT_TAG        ${CUBE_VERSION}
+      GIT_PROGRESS   TRUE
+   )
+   FetchContent_Declare(
+      STM32-CMSIS-${FAMILY}
+      GIT_REPOSITORY https://github.com/STMicroelectronics/cmsis_device_${FAMILY_L}/
+      GIT_TAG        ${CMSIS_VERSION}
+      GIT_PROGRESS   TRUE
+   )
+   FetchContent_Declare(
+      STM32-HAL-${FAMILY}
+      GIT_REPOSITORY https://github.com/STMicroelectronics/stm32${FAMILY_L}xx_hal_driver/
+      GIT_TAG        ${HAL_VERSION}
+      GIT_PROGRESS   TRUE
+   )
+   math(EXPR IDX "${IDX} + 1")
 endforeach()
 
 function(stm32_fetch_cube)
     foreach(FAMILY ${ARGV})
         set(CUBE_NAME STM32Cube${FAMILY})
         string(TOLOWER ${CUBE_NAME} CUBE_NAME_L)
-        
+
         if(STM32_CUBE_${FAMILY}_PATH)
             message(INFO "STM32_CUBE_${FAMILY}_PATH specified, skipping fetch for ${CUBE_NAME}")
             continue()
         endif()
-        
-		FetchContent_GetProperties(${CUBE_NAME} POPULATED CUBE_POPULATED)
+
+      FetchContent_GetProperties(${CUBE_NAME} POPULATED CUBE_POPULATED)
         if(NOT CUBE_POPULATED)
             set(FETCHCONTENT_QUIET FALSE) # To see progress
             FetchContent_Populate(${CUBE_NAME})
         endif()
-        
+
         set(STM32_CUBE_${FAMILY}_PATH ${${CUBE_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
     endforeach()
 endfunction()
 
 function(stm32_fetch_cmsis)
-	if(NOT STM32_CMSIS_PATH)
+   if(NOT STM32_CMSIS_PATH)
         if(NOT STM32-CMSIS_POPULATED)
             set(FETCHCONTENT_QUIET FALSE) # To see progress
             FetchContent_Populate(STM32-CMSIS)
         endif()
-        
+
         set(STM32_CMSIS_PATH ${stm32-cmsis_SOURCE_DIR} PARENT_SCOPE)
-	else()
-		message(INFO "STM32_CMSIS_PATH specified, skipping fetch for STM32-CMSIS")
-	endif()
+   else()
+      message(INFO "STM32_CMSIS_PATH specified, skipping fetch for STM32-CMSIS")
+   endif()
     foreach(FAMILY ${ARGV})
         set(CMSIS_NAME STM32-CMSIS-${FAMILY})
         string(TOLOWER ${CMSIS_NAME} CMSIS_NAME_L)
-        
+
         if(STM32_CMSIS_${FAMILY}_PATH)
             message(INFO "STM32_CMSIS_${FAMILY}_PATH specified, skipping fetch for ${CMSIS_NAME}")
             continue()
         endif()
-        
-		FetchContent_GetProperties(${CMSIS_NAME_L} POPULATED CMSIS_POPULATED)
+
+      FetchContent_GetProperties(${CMSIS_NAME_L} POPULATED CMSIS_POPULATED)
         if(NOT CMSIS_POPULATED)
             set(FETCHCONTENT_QUIET FALSE) # To see progress
             FetchContent_Populate(${CMSIS_NAME})
         endif()
-        
+
         set(STM32_CMSIS_${FAMILY}_PATH ${${CMSIS_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
     endforeach()
 endfunction()
@@ -136,18 +136,18 @@ function(stm32_fetch_hal)
     foreach(FAMILY ${ARGV})
         set(HAL_NAME STM32-HAL-${FAMILY})
         string(TOLOWER ${HAL_NAME} HAL_NAME_L)
-        
+
         if(STM32_HAL_${FAMILY}_PATH)
             message(INFO "STM32_HAL_${FAMILY}_PATH specified, skipping fetch for ${HAL_NAME}")
             continue()
         endif()
-        
-		FetchContent_GetProperties(${HAL_NAME} POPULATED HAL_POPULATED)
+
+      FetchContent_GetProperties(${HAL_NAME} POPULATED HAL_POPULATED)
         if(NOT HAL_POPULATED)
             set(FETCHCONTENT_QUIET FALSE) # To see progress
             FetchContent_Populate(${HAL_NAME})
         endif()
-        
+
         set(STM32_HAL_${FAMILY}_PATH ${${HAL_NAME_L}_SOURCE_DIR} PARENT_SCOPE)
     endforeach()
 endfunction()
