42a74dc1863ed04745342fcbff362624377ad071
ashen1queter <mnojasish@gmail.com>
2026-01-10 23:07:10 +0530

    change in pr #352

--- cmake/stm32/devices.cmake
+++ cmake/stm32/devices.cmake
@@ -183,7 +183,7 @@ endif()
 FetchContent_Declare(
     STM32-CMSIS
     GIT_REPOSITORY https://github.com/STMicroelectronics/cmsis_core/
-    GIT_TAG        v5.6.0
+    GIT_TAG        ${CMSIS_VERSION_TO_FETCH}
     GIT_PROGRESS   TRUE
 )
 
